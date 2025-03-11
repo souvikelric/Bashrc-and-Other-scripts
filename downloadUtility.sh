9b694fa49e387f74393c3ef23939a6fa443dc7c1
ROY CHOWDHURY <souvikelric@gmail.com>
2025-03-11 18:27:58 +0530

    Added colors

--- downloadUtility.sh
+++ downloadUtility.sh
@@ -1,6 +1,12 @@
 #! /bin/bash
 
 # echo "This is a utility for looking at the download folder and all of its files"
+
+#color variables
+cyan="\x1b[38;2;97;243;243m"
+yellow="\x1b[38;2;238;227;111m"
+reset="\x1b[0m"
+
 while true; do
   read -erp "Please enter the path to your downloads folder: " path
   if [ -d "$path" ]; then
@@ -10,11 +16,11 @@ while true; do
     hidden_files=$((all_files - visible_files))
     folder_count=$(find "$path" -maxdepth 1 -type d ! -path "$path" | wc -l)
     file_count=$(find "$path" -maxdepth 1 -type f | wc -l)
-    echo "Number of files and folders present in provided folder is $all_files"
-    echo "Number of visible files and folder is $visible_files"
-    echo "Number of hidden files and folder is $hidden_files"
-    echo "🗒️  Number of files : $file_count"
-    echo "📁 Number of folder : $folder_count"
+    echo -e "Number of files and folders present in provided folder:  ${yellow}$all_files${reset}"
+    echo -e "Number of visible files and folder:  ${yellow}$visible_files${reset}"
+    echo -e "Number of hidden files and folder is:  ${yellow}$hidden_files ${reset}"
+    echo -e "${cyan}🗒️  Number of files : $file_count${reset}"
+    echo -e "${cyan}📁 Number of folder : $folder_count${reset}"
     break
   else
     echo "Invalid folder path. Please try again."
